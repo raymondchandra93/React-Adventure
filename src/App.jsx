08b33b7820421b1f53475fb27ed88e2194f0ebc2
Raymond Chandra <raymond.schandra@gmail.com>
2025-05-02 12:49:03 -0400

    Import img from import statement and JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import react_img from './assets/react-core-concepts.png';
+
+const reactDescriptions = ['Core', 'Fundamental', 'Important'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function App() {
+  // Make the path leaner
+  const description = reactDescriptions[getRandomInt(reactDescriptions.length)];
+
+  // Return statement
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={react_img} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
